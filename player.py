09f63b79892a6786d6657a55f861b0b5077bff79
JustaSeall <gabyguerra2007@gmail.com>
2025-04-29 20:46:14 +0200

    Create player.py
    class player

---
+++ player.py
@@ -0,0 +1,19 @@
+import discord
+class Player():
+    """Classe joueur"""
+    def __init__(self, name, id: int, role, state: bool, member: discord.Member, nvote: int = 0, previous_vote = None, amour : bool = False):
+        self.name = name
+        self.id = id
+        self.role = role
+        self.state = state
+        self.member = member
+        self.nvote = nvote
+        self.previous_vote = previous_vote
+        self.amour = amour
+
+
+    async def is_alive(self):
+        if self.state:
+            return self.state
+        else:
+            await self.name.edit(mute=True)
