c00bf9f314be99c5ebc813ea350c12416a7e3102
JustaSeall <gabyguerra2007@gmail.com>
2025-04-29 20:46:52 +0200

    Delete client.py

--- client.py
+++
@@ -1,7 +0,0 @@
-import discord
-
-token = 'MTM1OTUxODk0OTA0MTExNTMyMA.GWVATN.B_qihYlFhcYWxI0NMrTVE-sI7-eDvQEKZgz6nM'
-
-client = discord.Client(intents=discord.Intents.all())
-
-client.run(token=token)
